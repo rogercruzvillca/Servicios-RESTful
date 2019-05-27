85b245f8fac641f4e4e587bef8722a7c2e9a9485
Roger <roger.cruz@myapps.com.bo>
2019-05-27 16:51:25 -0400

    Cliente de servicio RESTful en java (metodo GET)

---
+++ JAVA/RESTfulClient/src/main/java/RESTfulClientApp.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RESTfulClientApp {
+
+	public static void main(String[] args) {
+		try {
+			String url = "Indicar URL de Servicio RESTful";
+			HttpURLConnection cx = RESTfulClientApp.metodoGet(url);
+			if (cx.getResponseCode() == 200) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(cx.getInputStream()));
+				String salida;
+				StringBuilder sb = new StringBuilder();
+				while ((salida = br.readLine()) != null) {
+					sb.append(salida);
+				}
+				System.out.println("Salida json " + sb.toString());
+			}else {
+				System.out.println("Salida json " + cx.getResponseCode());
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static HttpURLConnection metodoGet(String url) throws IOException {
+		URL urlRETS = new URL(url);
+		HttpURLConnection cx = (HttpURLConnection) urlRETS.openConnection();
+		cx.setRequestMethod("GET");
+		cx.setDoOutput(false);
+		cx.setRequestProperty("Content-Type", "application/json");
+		cx.setRequestProperty("Authorization", "Basic cm9nZXIuY3J1ekBteWFwcHMuY29tLmJvOk15QXBwczIwMTkq");
+		return cx;
+	}
+
+}
